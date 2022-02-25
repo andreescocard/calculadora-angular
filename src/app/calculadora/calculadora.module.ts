95fa81d38c6bf0028fc04786317f056d331def6f
André <andre.escocard@hesea.com.br>
2022-02-24 23:52:17 -0300

    init repo

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculadoraComponent } from './components';
+
+
+
+@NgModule({
+  declarations: [
+    CalculadoraComponent
+  ],
+  imports: [
+    CommonModule
+  ], 
+  exports: [
+    CalculadoraComponent
+  ]
+})
+export class CalculadoraModule { }
