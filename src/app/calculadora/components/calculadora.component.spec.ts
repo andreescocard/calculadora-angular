3fb42ec4b82ea513dda3e68e0c248aabab15af72
André <andre.escocard@hesea.com.br>
2022-02-25 00:39:49 -0300

    feat: add unit test

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -2,13 +2,18 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalculadoraComponent } from './calculadora.component';
 
+import { CalculadoraService } from '../services'
+
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
   let fixture: ComponentFixture<CalculadoraComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CalculadoraComponent ]
+      declarations: [ CalculadoraComponent ],
+      providers: [
+        CalculadoraService
+      ]
     })
     .compileComponents();
   });
